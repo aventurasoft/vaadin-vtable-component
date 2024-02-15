9bde997843eb48b3ecf220bd4b9e5c63a15b2c41
arrojulio <arro_j@hotmail.com>
2024-02-15 00:28:10 -0300

    Add new Vaadin components and .gitignore
    This commit introduces new Vaadin components, 'App.java' and 'PivotTable.java', into the project. `App.java` contains the main method for application startup, while `PivotTable.java` sets up a dynamic, functional table for the app's frontend. A .gitignore file is also created to filter out unnecessary files.

---
+++ src/main/java/com/inttao/apps/vaadin/components/vtable/App.java
@@ -0,0 +1,8 @@
+package com.inttao.apps.vaadin.components.vtable;
+
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/inttao/apps/vaadin/components/vtable/PivotTable.java
@@ -0,0 +1,71 @@
+package com.inttao.apps.vaadin.components.vtable;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Div;
+
+import java.util.UUID;
+@JsModule("./vtable.min.js")
+public class PivotTable extends Div {
+    private String elementId;
+    private String data = "[]";
+    public PivotTable() {
+        UUID uuid = UUID.randomUUID();
+        this.elementId = uuid.toString();
+        setId(elementId);
+        UI.getCurrent().getPage().addJavaScript("./vtable.min.js");
+        setMinHeight("400px");
+        setMinWidth("600px");
+    }
+    public void setupTable() {
+
+        getElement().executeJs("const data="+ data + ";\n" +
+                "\n" +
+                "const option = {\n" +
+                "  records:data,\n" +
+                "  rows: [\"country\",\"customer\"],\n" +
+                "  columns: [ \"product\", \"sub product\"],\n" +
+                "  indicators: [\n" +
+                "                {\n" +
+                "                    \"indicatorKey\": \"amount\",\n" +
+                "                    \"title\": \"Amount\",\n" +
+                "                    \"width\": \"auto\",\n" +
+                "                    \"showSort\": false,\n" +
+                "                    \"headerStyle\":{\n" +
+                "                      fontWeight: \"normal\",\n" +
+                "                    },\n" +
+                "                    \"format\":(value)=>{\n" +
+                "                      if(value)\n" +
+                "                        return '$'+Number(value).toFixed(2);\n" +
+                "                      return '';\n" +
+                "                    },\n" +
+                "                    style:{\n" +
+                "                      padding:[16,28,16,28],\n" +
+                "                      color(args){\n" +
+                "                        if(args.dataValue>=0)\n" +
+                "                        return 'black';\n" +
+                "                        return 'red'\n" +
+                "                      }\n" +
+                "                     }\n" +
+                "                }\n" +
+                "  ],\n" +
+                "  \"corner\": {\n" +
+                "        \"titleOnDimension\": \"row\",\n" +
+                "        \"headerStyle\": {\n" +
+                "            \"textStick\": true\n" +
+                "    }\n" +
+                "  },\n" +
+                "  widthMode:'standard',\n" +
+                "  enableDataAnalysis: true,\n" +
+                "  defaultHeaderColWidth:150,\n" +
+                "  rowHierarchyIndent: 20\n" +
+                "};\n" +
+                "const tableInstance = new VTable.PivotTable(document.getElementById(\""+ this.elementId + "\"), option);\n" +
+                "\n" +
+                "\n");
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+
+}
