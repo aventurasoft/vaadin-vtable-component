40213d5b74546565b51acc0f072fe2d6612350e6
arrojulio <arro_j@hotmail.com>
2024-02-16 04:17:29 -0300

    Implement dark theme and add frontend routing
    In this commit, we added a Lumo dark theme to our App class using Vaadin's theme annotation. Additionally, a frontend routing functionality was initialized with an auto-generated index.ts file. The new file will help handle both client-side and server-side navigation in our application.

--- src/main/java/com/inttao/apps/vaadin/components/vtable/App.java
+++ src/main/java/com/inttao/apps/vaadin/components/vtable/App.java
@@ -1,5 +1,9 @@
 package com.inttao.apps.vaadin.components.vtable;
 
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@Theme(value = "myapp", variant = Lumo.DARK)
 public class App {
     public static void main( String[] args )
     {
